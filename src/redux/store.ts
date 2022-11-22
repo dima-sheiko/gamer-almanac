be2acf09c347a55e2e6343af0bd0d7dd572c133f
dima-sheiko <sheydim@gmail.com>
2022-11-22 14:28:51 +0300

    Add filter reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import gamesReducer from './reducers/gamesSlice';
+import filterReducer from './reducers/filterSlice';
 
 export const store = configureStore({
   reducer: {
     games: gamesReducer,
+    filter: filterReducer,
   },
 });
 
