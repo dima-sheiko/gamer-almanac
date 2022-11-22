449b90fee43b75b4759bd2f8ce0fa8cba1ce7c4e
dima-sheiko <sheydim@gmail.com>
2022-11-22 14:15:41 +0300

    Add reducer

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import gamesReducer from './reducers/gamesSlice';
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    games: gamesReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
