99e3f8b0e21720311dc012d021e5e6006131a545
dima-sheiko <sheydim@gmail.com>
2022-11-23 13:53:07 +0300

    Create utils folder & options lists

---
+++ src/utils/FilterOptions.ts
@@ -0,0 +1,14 @@
+import { TFilterOptions } from "../types/TFilterOptions";
+
+
+export const filterOptions: TFilterOptions[] = [
+  { id: 1, name: 'All Genres', value: '' },
+  { id: 2, name: 'Action', value: 'Action' },
+  { id: 3, name: 'Shooter', value: 'Shooter' },
+  { id: 4, name: 'RPG', value: 'RPG' },
+  { id: 5, name: 'Metroidvania', value: 'Metroidvania' },
+  { id: 6, name: 'Soulslike', value: 'Soulslike' },
+  { id: 7, name: 'Survival Horror', value: 'Survival Horror' },
+  { id: 8, name: 'Rogue Like', value: 'Rogue Like' },
+  { id: 9, name: 'Quest', value: 'Quest' },
+];
---
+++ src/utils/SortOptions.ts
@@ -0,0 +1,7 @@
+import { TSortOptions } from '../types/TSortOptions';
+
+export const sortOptions: TSortOptions[] = [
+  { id: 1, name: 'Title', value: 'title' },
+  { id: 2, name: 'Developer', value: 'developer' },
+  { id: 3, name: 'Year', value: 'year' },
+];
