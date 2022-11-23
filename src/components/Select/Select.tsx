97a05d3298ce3e9102cbce9c953aca1ba4415b85
dima-sheiko <sheydim@gmail.com>
2022-11-23 14:12:28 +0300

    Write reusable select logic

--- src/components/Select/Select.tsx
+++ src/components/Select/Select.tsx
@@ -1,3 +1,37 @@
-export const Select = () => {
-  return <div>Select</div>;
+import { AnyAction } from '@reduxjs/toolkit';
+import { useAppDispatch } from '../../hooks/redux';
+
+type Options = {
+  id: number;
+  name: string;
+  value: string;
+};
+
+type SelectProps = {
+  id: string;
+  name: string;
+  label: string;
+  options: Options[];
+  setValue: (e: string) => AnyAction;
+};
+
+export const Select = ({ id, name, label, options, setValue }: SelectProps) => {
+  const dispatch = useAppDispatch();
+
+  const onChangeSelect = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    dispatch(setValue(e.target.value));
+  };
+
+  return (
+    <div>
+      <label htmlFor={id}>{label}</label>
+      <select name={name} id={id} onChange={onChangeSelect}>
+        {options.map((option) => (
+          <option key={option.id} value={option.value}>
+            {option.name}
+          </option>
+        ))}
+      </select>
+    </div>
+  );
 };
