f267c6a960d34709c3a2c43f23d38eadd2dfcac2
dima-sheiko <sheydim@gmail.com>
2022-11-23 21:06:22 +0300

    Update API request structure

--- src/redux/reducers/gamesSlice.ts
+++ src/redux/reducers/gamesSlice.ts
@@ -2,6 +2,11 @@ import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import axios from 'axios';
 import { TGames } from '../../types/TGames';
 
+type FetchGamesParams = {
+  filterParam: string;
+  sortParam: string;
+};
+
 type GamesState = {
   games: TGames[];
   isLoading: boolean;
@@ -14,16 +19,20 @@ const initialState: GamesState = {
   error: '',
 };
 
-export const fetchGames = createAsyncThunk('games/fetchGames', async (_, thunkAPI) => {
-  try {
-    const response = await axios.get<TGames[]>(
-      `https://637bace46f4024eac21566d8.mockapi.io/games`
-    );
-    return response.data;
-  } catch (error) {
-    return thunkAPI.rejectWithValue(error)
+export const fetchGames = createAsyncThunk(
+  'games/fetchGames',
+  async (params: FetchGamesParams, thunkAPI) => {
+    const { filterParam, sortParam } = params;
+    try {
+      const response = await axios.get<TGames[]>(
+        `https://637bace46f4024eac21566d8.mockapi.io/games?&genre=${filterParam}&sortBy=${sortParam}`
+      );
+      return response.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
-});
+);
 
 const gamesSlice = createSlice({
   name: 'games',
