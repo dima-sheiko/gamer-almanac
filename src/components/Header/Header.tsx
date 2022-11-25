67e1ffc9dd629941a30d35e5e8ac8e341da33edc
dima-sheiko <sheydim@gmail.com>
2022-11-25 17:28:14 +0300

    Style header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,9 @@
+import styles from './Header.module.css';
+
 type HeaderProps = {
   title: string;
 };
 
 export const Header = ({ title }: HeaderProps) => {
-  return <header>{title}</header>;
+  return <header className={styles.header}>{title}</header>;
 };
