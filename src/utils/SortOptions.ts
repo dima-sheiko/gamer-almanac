0636afeb256ff66d0b5184f07c17132dae46ef63
Дмитрий Шейко <sheydim@gmail.com>
2022-12-15 19:00:20 +0300

    Add enum to properties

--- src/utils/SortOptions.ts
+++ src/utils/SortOptions.ts
@@ -1,7 +1,8 @@
+import { ESortParam } from '../types/ESortParam';
 import { TSortOptions } from '../types/TSortOptions';
 
 export const sortOptions: TSortOptions[] = [
-  { id: 1, name: 'Title', value: 'title' },
-  { id: 2, name: 'Developer', value: 'developer' },
-  { id: 3, name: 'Year', value: 'year' },
+  { id: 1, name: 'Title', value: ESortParam.Title },
+  { id: 2, name: 'Developer', value: ESortParam.Developer },
+  { id: 3, name: 'Year', value: ESortParam.Year },
 ];
