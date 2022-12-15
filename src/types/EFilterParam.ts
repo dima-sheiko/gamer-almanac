771d206a517ab1ff8c08942f7b72ae909dacd6e1
Дмитрий Шейко <sheydim@gmail.com>
2022-12-15 14:44:25 +0300

    Create enum

---
+++ src/types/EFilterParam.ts
@@ -0,0 +1,11 @@
+export enum EFilterParam {
+  All = '',
+  Action = 'Action',
+  Shooter = 'Shooter',
+  RPG = 'RPG',
+  Metroidvania = 'Metroidvania',
+  Soulslike = 'Soulslike',
+  Survival_Horror = 'Survival Horror',
+  Rogue_Like = 'Rogue Like',
+  Quest = 'Quest',
+}
